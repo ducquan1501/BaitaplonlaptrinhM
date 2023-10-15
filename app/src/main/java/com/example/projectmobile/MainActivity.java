2de5cedd59f7f922d06e74453053c7b702ac6103
Quan <michealangel1501@gmail.com>
2023-10-16 02:20:51 +0700

    Làm giao diện & vẽ đồ thị dạng Hyperbol + Làm Fragment đầu tiên.

--- app/src/main/java/com/example/projectmobile/MainActivity.java
+++ app/src/main/java/com/example/projectmobile/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
@@ -22,6 +23,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -36,14 +39,13 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.utils.ViewPortHandler;
+import com.google.android.material.navigation.NavigationView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.Math;
-public class MainActivity extends AppCompatActivity {
-    private EditText editText1, editText2, editText3, editText4, editText5;
-    public Button menuButton;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public ActionBarDrawerToggle toggle;
     public DrawerLayout drawerLayout;
 
@@ -51,183 +53,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //1. Gọi các thành phần từ Layout, thiết lập tham số
-        LineChart lineChart = findViewById(R.id.chart);
-        editText1 = findViewById(R.id.parameter1);
-        editText2 = findViewById(R.id.parameter2);
-        editText3 = findViewById(R.id.parameter3);
-        editText4 = findViewById(R.id.parameter4);
-        editText5 = findViewById(R.id.parameter5);
-
-        Button executeB = findViewById(R.id.executeB);
-        Button deleteB = findViewById(R.id.deleteButton);
-        Button undoB = findViewById(R.id.undoB);
-
         drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
-        toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
-        drawerLayout.addDrawerListener(toggle);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Đồ thị hàm cơ bản");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+        toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
+        drawerLayout.addDrawerListener(toggle);
 
-        Toolbar kebab_Menu = findViewById(R.id.kebabMenu);
-        setSupportActionBar(kebab_Menu);
-
-        //2. Tạo đường trục tung, trục hoành
-        //2.1. Xây dựng Entry - tập hợp các phần tử để tạo thành đồ thị
-        ArrayList<Entry> zeroLineEntriesX = new ArrayList<>();
-        for (float x0 = -55; x0 <= 55; x0 += 0.1) {
-            float y0 = 0;
-            zeroLineEntriesX.add(new Entry(x0, y0));
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new Basic_Graph())
+                    .commit();
+            navigationView.setCheckedItem(R.id.basic_graph);
         }
-
-        ArrayList<Entry> zeroLineEntriesY = new ArrayList<>();
-        zeroLineEntriesY.add(new Entry(0f, 100f));
-        zeroLineEntriesY.add(new Entry(0f, -100f));
-
-        //2. Tạo DataSet cho các đường trục tung, trục hoành
-        LineDataSet zeroLineDataSetX = new LineDataSet(zeroLineEntriesX, "Ox");
-        zeroLineDataSetX.setDrawValues(false);
-        zeroLineDataSetX.setDrawCircles(false);
-        zeroLineDataSetX.setColor(Color.BLACK);
-        zeroLineDataSetX.setLineWidth(3f);
-
-        LineDataSet zeroLineDataSetY = new LineDataSet(zeroLineEntriesY, "Oy");
-        zeroLineDataSetX.setDrawValues(false);
-        zeroLineDataSetY.setDrawCircles(false);
-        zeroLineDataSetY.setColor(Color.BLACK);
-        zeroLineDataSetY.setLineWidth(3f);
-
-        LineData lineData = new LineData(zeroLineDataSetX, zeroLineDataSetY);
-
-        //3. Cấu hình đồ thị
-        XAxis xAxis = lineChart.getXAxis();
-        xAxis.setDrawLabels(true);
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setAxisMaximum(55f);
-        xAxis.setAxisMinimum(-55f);
-        xAxis.setDrawGridLines(true);
-        xAxis.setGranularity(0.1f);
-        xAxis.setLabelCount(10);
-//        xAxis.setValueFormatter(new ValueFormatter() {
-//            @Override
-//            public String getAxisLabel(float value, AxisBase axis) {
-//                return String.valueOf(value);
-//            }
-//        });
-
-        YAxis yAxis = lineChart.getAxisLeft();
-        YAxis yAxisR = lineChart.getAxisRight();
-        yAxisR.setEnabled(false);
-        yAxis.setDrawLabels(true);
-        yAxis.setAxisMaximum(100f);
-        yAxis.setAxisMinimum(-100f);
-        yAxis.setGranularity(0.1f);
-        yAxis.setLabelCount(16);
-
-        lineChart.setData(lineData);
-        Legend lg = lineChart.getLegend();
-        lg.setEnabled(false);
-
-        Description dp = new Description();
-        dp.setText("Nguyễn Đức Quân CNTT 14-04");
-        dp.setTextColor(Color.BLUE);
-        dp.setTextSize(10f);
-        lineChart.setDescription(dp);
-
-        lineChart.setSaveEnabled(true);
-        lineChart.setPinchZoom(true);
-        lineChart.invalidate();
-
-        //4. Thiết lập chức năng cho nút Button
-        executeB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //4.1. Xử lý các editText trống
-                String et1 = editText1.getText().toString();
-                if (et1.isEmpty() || et1.equals("-") || et1.equals(".")) {
-                    editText1.setText("0");
-                }
-                String et2 = editText2.getText().toString();
-                if (et2.isEmpty() || et2.equals("-") || et2.equals(".")) {
-                    editText2.setText("0");
-                }
-                String et3 = editText3.getText().toString();
-                if (et3.isEmpty() || et3.equals("-") || et3.equals(".")) {
-                    editText3.setText("0");
-                }
-                String et4 = editText4.getText().toString();
-                if (et4.isEmpty() || et4.equals("-") || et4.equals(".")) {
-                    editText4.setText("0");
-                }
-                String et5 = editText5.getText().toString();
-                if (et5.isEmpty() || et5.equals("-") || et5.equals(".")) {
-                    editText5.setText("0");
-                }
-                Float p1 = Float.parseFloat(editText1.getText().toString());
-                Float p2 = Float.parseFloat(editText2.getText().toString());
-                Float p3 = Float.parseFloat(editText3.getText().toString());
-                Float p4 = Float.parseFloat(editText4.getText().toString());
-                Float p5 = Float.parseFloat(editText5.getText().toString());
-
-                //4.2. Tạo mảng chứa tập hợp các điểm hình thành nên đồ thị
-                List<Entry> entries = new ArrayList<>();
-                for (float x = -55; x <= 55; x += 0.01) {
-                    float y = p1 * x * x * x * x + p2 * x * x * x + p3 * x * x + p4 * x + p5;
-                    entries.add(new Entry(x, y));
-                }
-                //4.3 Khai báo dataSet
-                LineDataSet dataSet = new LineDataSet(entries, "y = " + p1 + "x^4 +" + p2 + "x^3 + " + p3 + "x^2 + " + p4 + "x+ " + p5);
-                int i = lineData.getDataSetCount();
-                if (i % 7 == 0) {
-                    dataSet.setColor(Color.YELLOW);
-                } else if (i % 7 == 1) {
-                    dataSet.setColor(Color.parseColor("#FFAE00"));
-                } else if (i % 7 == 2) {
-                    dataSet.setColor(Color.RED);
-                } else if (i % 7 == 3) {
-                    dataSet.setColor(Color.GREEN);
-                } else if (i % 7 == 4) {
-                    dataSet.setColor(Color.parseColor("#02C7FC"));
-                } else if (i % 7 == 5) {
-                    dataSet.setColor(Color.parseColor("#2E00FF"));
-                } else if (i % 7 == 6) {
-                    dataSet.setColor(Color.parseColor("#A320D5"));
-                }
-                dataSet.setDrawValues(false);
-                dataSet.setDrawCircles(false);
-                dataSet.setLineWidth(2f);
-
-                //4.4. Tạo một LineData chứa LineDataSet của đường kẻ
-                lineData.addDataSet(dataSet);
-                lineChart.setData(lineData);
-
-                //4.5. Vẽ đồ thị
-                lineChart.invalidate();
-            }
-        });
-        //4.6 Xóa editText
-        deleteB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                editText1.setText("");
-                editText2.setText("");
-                editText3.setText("");
-                editText4.setText("");
-                editText5.setText("");
-            }
-        });
-        //4.7. Nút Undo
-        undoB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int i = lineData.getDataSetCount();
-                if (i > 2) {
-                    lineData.removeDataSet(i - 1);
-                }
-                lineChart.invalidate();
-            }
-        });
     }
 
     @Override
@@ -235,21 +79,35 @@ public class MainActivity extends AppCompatActivity {
         if (toggle.onOptionsItemSelected(item)) {
             return true;
         }
-        int id = item.getItemId();
-
-        if(id==R.id.item1) {
-            return true;
-        } else if (id==R.id.item2) {
-            return true;
-        } else if (id==R.id.item3) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
         return super.onOptionsItemSelected(item);
-        }
+    }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.sub_menu, menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.basic_graph) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new Basic_Graph())
+                    .commit();
+            getSupportActionBar().setTitle("Đồ thị hàm cơ bản");
+        } else if (item.getItemId() == R.id.hyperbol_graph) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new Hyperbol_Graph())
+                    .commit();
+            getSupportActionBar().setTitle("Đồ thị dạng Hyperbol");
+        }
+        drawerLayout.closeDrawer(GravityCompat.START, true);
         return true;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        toggle.syncState();
     }
 }
