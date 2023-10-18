2669645e306d969dcc6f46f36d497b84133fb5c9
Quan <michealangel1501@gmail.com>
2023-10-18 20:46:00 +0700

    Làm splash screen

--- app/src/main/java/com/example/projectmobile/SplashActivity.java
+++ app/src/main/java/com/example/projectmobile/SplashActivity.java
@@ -1,4 +1,24 @@
 package com.example.projectmobile;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 4444);
+        // Thêm mã xử lý nếu cần
+    }
 }
